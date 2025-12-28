2784fd744073991edbb34992493c2835330acd50
Mazid Ishtique <rae@adust.edu.bd>
2025-12-28 10:25:37 +0600

    Fix page heading visibility for mobile and tablet

--- app/segments/page.tsx
+++ app/segments/page.tsx
@@ -9,7 +9,7 @@ export default function Segments() {
 
         <div className="flex justify-center mt-2 mb-8">
           <span className="h-1 w-20 bg-technorange rounded"></span>
-        </div>
+        npm run dev</div>
 
         <p className="text-center text-gray-600 mb-10">
           Event details are as following (subject to revision by the organizers)
@@ -17,7 +17,7 @@ export default function Segments() {
 
         {/* DAY 1 */}
         <section className="mb-16">
-          <h2 className="text-3xl font-bold mb-6">
+          <h2 className="text-lg sm:text-xl md:text-2xl font-semibold text-gray-800">
             🔥 Day 1: Tech Innovation & Knowledge Fest
           </h2>
 
@@ -113,7 +113,7 @@ export default function Segments() {
 
         {/* DAY 2 */}
         <section>
-          <h2 className="text-3xl font-bold mb-6">
+          <h2 className="text-lg sm:text-xl md:text-2xl font-semibold text-gray-800">
             🎉 Day 2: Culture, Fun & Connection
           </h2>
 
